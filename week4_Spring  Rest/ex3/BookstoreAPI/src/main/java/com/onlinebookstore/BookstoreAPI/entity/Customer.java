53c69a5303c89b107ea3c8d19f0df84363f272c0
theo savio <118189819+theosavio@users.noreply.github.com>
2024-08-22 23:36:31 +0530

    Add files via upload

---
+++ week4_Spring  Rest/ex3/BookstoreAPI/src/main/java/com/onlinebookstore/BookstoreAPI/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.onlinebookstore.BookstoreAPI.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
